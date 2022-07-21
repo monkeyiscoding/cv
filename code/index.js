0c331973550655b6b32b202454aaa123d5a62861
monkeyiscoding <109468448+monkeyiscoding@users.noreply.github.com>
2022-07-21 15:42:43 +0530

    0000.0

--- code/index.js
+++ code/index.js
@@ -1,6 +1,18 @@
 
-document.getElementById("explore").addEventListener("click", function () {
-  // location.href ="explore.html"
-  var dogAudio = new Audio("sounds/dog.wav");
-  dogAudio.play();
+// $("explore").addEventListener("click", function () {
+//   // location.href ="explore.html"
+//   var dogAudio = new Audio("sounds/dog.wav");
+//   dogAudio.play();
+// });
+
+$("#explore").click(function(){
+  $("h1").css("color","purple");
+});
+
+$("#message").click(function(){
+  $(".messageDialog").css("display","block")
+});
+
+$("#closedialog").click(function(){
+  $("#dialog").css("display","none");
 });
